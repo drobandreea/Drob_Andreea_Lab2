5f218849762bfc966b2964d5a6d4c2015f6258ba
Andreea <Andreea@DESKTOP-8M0J4D7>
2022-10-26 22:16:19 -1200

    Add project files.

---
+++ Drob_Andreea_Lab2/Data/Drob_Andreea_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Drob_Andreea_Lab2.Models;
+
+namespace Drob_Andreea_Lab2.Data
+{
+    public class Drob_Andreea_Lab2Context : DbContext
+    {
+        public Drob_Andreea_Lab2Context (DbContextOptions<Drob_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Drob_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Drob_Andreea_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Drob_Andreea_Lab2.Models.Author> Author { get; set; }
+    }
+}
